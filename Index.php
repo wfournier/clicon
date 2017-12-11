7488daf4a3cdcceb368e51fe27f19fa13e04d6a2
wfournier <wfournier2710@gmail.com>
2017-12-11 11:00:02 -0500

    added connection to database

--- Index.php
+++ Index.php
@@ -1,14 +1,27 @@
+<?php
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="gamecon";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include "Shared/Head.html" ?>
-    <title>$Title$</title>
+	<?php include "Shared/Head.html" ?>
+	<title>$Title$</title>
 </head>
 <body>
-<?php include "Shared/Header.html" ?>
+	<?php include "Shared/Header.html" ?>
 
-<p>Test</p>
+	<p>Test</p>
 
-<?php include "Shared/Footer.html" ?>
+	<?php include "Shared/Footer.html" ?>
 </body>
 </html>
