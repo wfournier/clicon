eb26c5d250b0ab204a772f85b867afbe790314a1
wfournier <wfournier2710@gmail.com>
2017-12-15 13:04:14 -0500

    added query to fill input fields in account_modify

--- Account_ModifyInfo.php
+++ Account_ModifyInfo.php
@@ -29,107 +29,100 @@ if (!func::checkLogin($con)) {
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="form">
                     <div class="row">
                         <div class="form-group col-xs-6">
-                            <label for="fname" class="<?php echo $fnameErr ? 'error' : '' ?>">First Name</label>
-                            <input type="text" class="form-control" id="fname" name="fname"
-                            value="<?php $account['FIRST_NAME'] ?>" data-toggle="tooltip" title="First Name must only contain letters">
+                            <label for="fnameMod" class="<?php echo $fnameModErr ? 'error' : '' ?>">First Name</label>
+                            <input type="text" class="form-control" id="fnameMod" name="fnameMod"
+                            value="<?php echo $fnameMod ?>" data-toggle="tooltip" title="First Name must only contain letters">
                         </div>
                         <div class="form-group col-xs-6">
-                            <label for="lname" class="<?php echo $lnameErr ? 'error' : '' ?>">Last Name</label>
-                            <input type="text" class="form-control" id="lname" name="lname"
-                            placeholder="Enter Last Name"
-                            value="<?php echo isset($_POST['lname']) ? $_POST['lname'] : '' ?>"
+                            <label for="lnameMod" class="<?php echo $lnameModErr ? 'error' : '' ?>">Last Name</label>
+                            <input type="text" class="form-control" id="lnameMod" name="lnameMod"
+                            value="<?php echo $lnameMod ?>"
                             data-toggle="tooltip" title="Last Name must only contain letters">
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-xs-6">
-                            <label for="phone" class="<?php echo $phoneErr ? 'error' : '' ?>">Phone Number</label>
-                            <input type="tel" class="form-control" id="phone" name="phone"
-                            placeholder="Enter Phone Number"
-                            value="<?php echo isset($_POST['phone']) ? $_POST['phone'] : '' ?>"
+                            <label for="phoneMod" class="<?php echo $phoneModErr ? 'error' : '' ?>">Phone Number</label>
+                            <input type="tel" class="form-control" id="phoneMod" name="phoneMod"
+                            value="<?php echo $phoneMod ?>"
                             data-toggle="tooltip" title="Phone number must be valid (i.e. 000-000-0000)">
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-xs-6">
-                            <label for="countryId"
-                            class="<?php echo $countryIdErr ? 'error' : '' ?>">Country</label>
-                            <select class="form-control" id="countryId" name="countryId"
-                            placeholder="Select Country" data-toggle="tooltip" title="">
-                            <?php
-                            $get_countries_sql = "SELECT * FROM COUNTRIES";
-                            $get_countries_res = $con->query($get_countries_sql) or die("get_countries_res: " . $con->error);
+                            <label for="countryIdMod"
+                            class="<?php echo $countryIdModErr ? 'error' : '' ?>">Country</label>
+                            <select class="form-control" id="countryIdMod" name="countryIdMod" data-toggle="tooltip" title="">
+                                <?php
+                                $get_countries_sql = "SELECT * FROM COUNTRIES";
+                                $get_countries_res = $con->query($get_countries_sql) or die("get_countries_res: " . $con->error);
 
-                            if ($get_countries_res->num_rows < 1) {
-                                echo("<p><i>Invalid selection</i></p>");
-                            } else {
-                                while ($country = $get_countries_res->fetch_array()) {
-                                    $id = $country["COUNTRY_ID"];
-                                    $countryCode = $country["COUNTRY_CODE"];
-                                    $name = $country["COUNTRY_NAME"];
-                                    $selected = "";
-                                    if (isset($_POST["countryId"])) {
-                                        if ($_POST["countryId"] == $id) {
+                                if ($get_countries_res->num_rows < 1) {
+                                    echo("<p><i>Invalid selection</i></p>");
+                                } else {
+                                    while ($country = $get_countries_res->fetch_array()) {
+                                        $id = $country["COUNTRY_ID"];
+                                        $countryCode = $country["COUNTRY_CODE"];
+                                        $name = $country["COUNTRY_NAME"];
+                                        $selected = "";
+                                        if ($countryIdMod == $id) {
                                             $selected = "selected";
                                         }
-                                    }
-                                    if ($name == "Canada" || $name == "United States") {
-                                        echo("<option value=\"" . $id . "\" " . $selected . ">" . $name . "</option>");
+                                        if ($name == "Canada" || $name == "United States") {
+                                            echo("<option value=\"" . $id . "\" " . $selected . ">" . $name . "</option>");
+                                        }
                                     }
                                 }
-                            }
-                            $get_countries_res->free();
-                            ?>
-                        </select>
+                                $get_countries_res->free();
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-xs-6">
+                            <label for="stateIdMod" class="<?php echo $stateIdModErr ? 'error' : '' ?>">State/Province</label>
+                            <input type="text" class="form-control" id="stateIdMod" name="stateIdMod" value="<?php echo $stateIdMod ?>" data-toggle="tooltip" title="State code must be 2 characters long">
+                        </div>
                     </div>
-                    <div class="form-group col-xs-6">
-                        <label for="state" class="<?php echo $stateErr ? 'error' : '' ?>">State/Province</label>
-                        <input type="text" class="form-control" id="state" name="state" value="<?php echo isset($_POST['state']) ? $_POST['state'] : '' ?>"
-                        data-toggle="tooltip" title="State code must be 2 characters long">
+                    <div class="row">
+                        <div class="form-group col-xs-6">
+                            <label for="cityMod" class="<?php echo $cityModErr ? 'error' : '' ?>">City</label>
+                            <input type="text" class="form-control" id="cityMod" name="cityMod" value="<?php echo $cityMod ?>"
+                            data-toggle="tooltip" title="City must only contain letters">
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-xs-6">
-                        <label for="city" class="<?php echo $cityErr ? 'error' : '' ?>">City</label>
-                        <input type="text" class="form-control" id="city" name="city" value="<?php echo isset($_POST['city']) ? $_POST['city'] : '' ?>"
-                        data-toggle="tooltip" title="City must only contain letters">
+                    <div class="row">
+                        <div class="form-group col-xs-6">
+                            <label for="addressMod" class="<?php echo $addressModErr ? 'error' : '' ?>">Address</label>
+                            <input type="text" class="form-control" id="addressMod" name="addressMod" value="<?php echo $addressMod ?>" data-toggle="tooltip" title="Address must be valid">
+                        </div>
+                        <div class="form-group col-xs-6">
+                            <label for="zipMod" class="<?php echo $zipModErr ? 'error' : '' ?>">Zip Code</label>
+                            <input type="text" class="form-control" id="zipMod" name="zipMod" value="<?php echo $zipMod ?>"
+                            data-toggle="tooltip" title="Zip code must be valid (i.e. A1B 2C3)">
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-xs-6">
-                        <label for="address" class="<?php echo $addressErr ? 'error' : '' ?>">Address</label>
-                        <input type="text" class="form-control" id="address" name="address" value="<?php echo isset($_POST['address']) ? $_POST['address'] : '' ?>" data-toggle="tooltip" title="Address must be valid">
+                    <hr>
+                    <div class="row">
+                        <div class="form-group col-xs-6">
+                            <label for="passMod" class="<?php echo $passModErr ? 'error' : '' ?>">Confirm Password</label>
+                            <input type="Password" class="form-control" id="passMod" name="passMod" data-toggle="tooltip"
+                            title="Password must contain be at least 8 characters long, contain numbers and at least 1 uppercase letter">
+                        </div>
                     </div>
-                    <div class="form-group col-xs-6">
-                        <label for="zip" class="<?php echo $zipErr ? 'error' : '' ?>">Zip Code</label>
-                        <input type="text" class="form-control" id="zip" name="zip" value="<?php echo isset($_POST['zip']) ? $_POST['zip'] : '' ?>"
-                        data-toggle="tooltip" title="Zip code must be valid (i.e. A1B 2C3)">
+                    <div class="row">
+                        <button type="submit" class="btn btn-success btn-lg">Save</button>
                     </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="form-group col-xs-6">
-                        <label for="pass" class="<?php echo $passErr ? 'error' : '' ?>">Password</label>
-                        <input type="Password" class="form-control" id="pass" name="pass" data-toggle="tooltip"
-                        title="Password must contain be at least 8 characters long, contain numbers and at least 1 uppercase letter">
+                    <div class="row" style="height: 80px; padding: 10px;">
+                        <p><span class="error"><?php echo($registerErrMsg); ?></span></p>
                     </div>
-                </div>
-                <div class="row">
-                    <button type="submit" class="btn btn-success btn-lg">Register</button>
-                    <button type="reset" class="btn btn-warning btn-lg">Reset</button>
-                </div>
-                <div class="row" style="height: 80px; padding: 10px;">
-                    <p><span class="error"><?php echo($registerErrMsg); ?></span></p>
-                </div>
-                <input type="hidden" name="process" value="register">
-            </form>
+                    <input type="hidden" name="process" value="modify">
+                </form>
+            </div>
+            <div class="col-sm-3">
+                <p><span class="success"><?php echo($registerOutput); ?></span></p>
+            </div>
         </div>
-        <div class="col-sm-3">
-            <p><span class="success"><?php echo($registerOutput); ?></span></p>
-        </div>
-    </div>
-</main>
-<?php include "Shared/Footer.html"; ?>
+    </main>
+    <?php include "Shared/Footer.html"; ?>
 </body>
 </html>
 
--- Processes/ModifyProcess.php
+++ Processes/ModifyProcess.php
@@ -1,9 +1,31 @@
 <?php
 
-$get_account_info_sql = "SELECT * FROM ACCOUNT WHERE ACCOUNT_ID = " .$_COOKIE['account_id'];
+$fnameMod = $lnameMod = $phoneMod = $countryIdMod = $stateIdMod = $cityMod = $addressMod = $zipMod = $passMod = "";
+$fnameModErr = $lnameModErr = $phoneModErr = $countryIdModErr = $stateIdModErr = $cityModErr = $addressModErr = $zipModErr = $passModErr = false;
+
+$modPassErr = false;
+$modErrMsg = $modOutput = "";
+$error = false;
+
+$get_account_info_sql = "SELECT a.FIRST_NAME, a.LAST_NAME, a.PHONE, a.COUNTRY_ID, s.STATE_CODE, a.CITY, a.ADDRESS, a.ZIP FROM ACCOUNT a, STATES s WHERE a.ACCOUNT_ID = " .$_COOKIE['account_id'] ." AND s.STATE_ID = a.STATE_ID";
 $get_account_info_res = $con->query($get_account_info_sql) or die("get_account_info_res: " .$con->error);
 
 while($account = $get_account_info_res->fetch_array()){
+	$fnameMod = $account["FIRST_NAME"];
+	$lnameMod = $account["LAST_NAME"];
+	$phoneMod = $account["PHONE"];
+	$countryIdMod = $account["COUNTRY_ID"];
+	$stateIdMod = $account["STATE_CODE"];
+	$cityMod = $account["CITY"];
+	$addressMod = $account["ADDRESS"];
+	$zipMod = $account["ZIP"];
+}
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if ($p["process"] == "modify") {
+
+
+
+	}
 }
 ?>
