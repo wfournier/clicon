45e39770f23aaa06b8638d979b1893f3bb44a8a6
SOMEONELAPTOP\luxor <luxor37@gmail.com>
2018-02-12 15:57:25 -0500

    Milestone 4.1

--- Shared/connection.php
+++ Shared/connection.php
@@ -6,7 +6,7 @@ $user="root";
 $password="root";
 $dbname="gamecon";
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+$con = new mysqli($host, $user, $password, $dbname)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
 //$con->close();
