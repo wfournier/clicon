0b4df1a2b043b65d6acdaef2eaf87c5c51d93af5
SOMEONEPC\luxor <luxor37@gmail.com>
2018-02-14 01:32:34 -0500

    testing5

--- Processes/CheckLogin.php
+++ Processes/CheckLogin.php
@@ -4,8 +4,9 @@ class func
 {
     public static function checkLogin($conn)
     {
+        print("<script>console.log('. json_encode( \"CheckLogin\" ) .')</script>");
         if (isset($_COOKIE['account_id']) && isset($_COOKIE['token'])) {
-
+            print("<script>console.log('. json_encode( \"cookies set\" ) .')</script>");
             $account_id = $_COOKIE['account_id'];
             $token = $_COOKIE['token'];
 
@@ -13,6 +14,7 @@ class func
             $results = $conn->query($query23) or die ("HELP " . $conn->error);
 
             if ($results->num_rows > 0) {
+                print("<script>console.log('. json_encode( \"query not empty\" ) .')</script>");
                 while ($result = $results->fetch_array()) {
                     if ($result['session_accountid'] == $_COOKIE['account_id'] &&
                         $result['session_token'] == $_COOKIE['token']) {
