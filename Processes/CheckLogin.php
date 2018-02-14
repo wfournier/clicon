b439fd84df76e2d2399419d80ae63f4e5b168a31
SOMEONEPC\luxor <luxor37@gmail.com>
2018-02-14 01:35:15 -0500

    testing6

--- Processes/CheckLogin.php
+++ Processes/CheckLogin.php
@@ -10,21 +10,19 @@ class func
             $account_id = $_COOKIE['account_id'];
             $token = $_COOKIE['token'];
 
-            $query23 = "SELECT * FROM sessions WHERE session_accountid = " . $account_id . " ;";
+            $query23 = "SELECT * FROM sessions WHERE session_accountid = " . $account_id . " AND session_token = '" . $token . "';";
             $results = $conn->query($query23) or die ("HELP " . $conn->error);
 
             if ($results->num_rows > 0) {
                 print("<script>console.log('. json_encode( \"query not empty\" ) .')</script>");
                 while ($result = $results->fetch_array()) {
-                    if ($result['session_accountid'] == $_COOKIE['account_id'] &&
-                        $result['session_token'] == $_COOKIE['token']) {
-                        print("<script>console.log('. json_encode( \"true\" ) .')</script>");
-                        return true;
-                    }
+                    print("<script>console.log('. json_encode( \"true\" ) .')</script>");
+                    return true;
                 }
             } else {
                 print("<script>console.log('. json_encode( \"no result from query\" ) .')</script>");
             }
         }
     }
-} ?>
+}
+?>
