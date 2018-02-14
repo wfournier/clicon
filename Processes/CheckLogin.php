fdd520633a971e7e344077a63f2dd071f4a16436
SOMEONEPC\luxor <luxor37@gmail.com>
2018-02-14 01:19:00 -0500

    testing1

--- Processes/CheckLogin.php
+++ Processes/CheckLogin.php
@@ -8,10 +8,10 @@ class func
             $account_id = $_COOKIE['account_id'];
             $token = $_COOKIE['token'];
 
-            $query23 = "SELECT * FROM sessions WHERE session_accountid = " . $account_id . " AND session_token = '" . $token . "';";
+            $query23 = "SELECT * FROM sessions WHERE session_accountid = " . $account_id . ";";
             $results = $conn->query($query23) or die ("HELP " . $conn->error);
 
-            if ($results->num_rows > 0) {
+            if ($results->num_rows == 1) {
                 while ($result = $results->fetch_array()) {
                     if ($result['session_accountid'] == $_COOKIE['account_id'] &&
                         $result['session_token'] == $_COOKIE['token']) {
