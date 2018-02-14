442df9968ce9aa27d027b89ecf37653a6d9592c3
SOMEONEPC\luxor <luxor37@gmail.com>
2018-02-14 01:20:46 -0500

    testing2

--- Processes/CheckLogin.php
+++ Processes/CheckLogin.php
@@ -11,16 +11,16 @@ class func
             $query23 = "SELECT * FROM sessions WHERE session_accountid = " . $account_id . ";";
             $results = $conn->query($query23) or die ("HELP " . $conn->error);
 
-            if ($results->num_rows == 1) {
+
                 while ($result = $results->fetch_array()) {
                     if ($result['session_accountid'] == $_COOKIE['account_id'] &&
                         $result['session_token'] == $_COOKIE['token']) {
                         return true;
                     }
                 }
-            } else {
+
                 print("<script>console.log('. json_encode( \"no cookie for connection, $account_id, $token\" ) .')</script>");
-            }
+
         }
     }
 } ?>
