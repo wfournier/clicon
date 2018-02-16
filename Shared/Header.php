d61ca696b674bef492be6244e36d0e3f563c1686
wfournier <wfournier2710@gmail.com>
2018-02-15 19:19:51 -0500

    added translation to header

--- Shared/Header.php
+++ Shared/Header.php
@@ -32,33 +32,33 @@
         <!--Left Side-->
         <div class="collapse navbar-collapse navBarText" id="myNavbar">
             <ul class="nav navbar-nav">
-                <li><a href="/Index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                <li><a href="/Index.php"><span class="glyphicon glyphicon-home"></span> <?php echo($lang("home")); ?></a></li>
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="navButtons">
-                        <span class="glyphicon glyphicon-info-sign"></span> Information
+                        <span class="glyphicon glyphicon-info-sign"></span> <?php echo($lang("info")); ?>
                         <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="/Information.php#hours">Opening Hours</a></li>
-                            <li><a href="/Information.php#location">Location</a></li>
-                            <li><a href="/Information.php#rules">Convention Rules</a></li>
-                            <li><a href="/Information.php#registration">Registration Policies</a></li>
-                            <li><a href="/Information.php#weapon">Weapon Policy</a></li>
-                            <li><a href="/Information.php#autograph">Autograph Policy</a></li>
+                            <li><a href="/Information.php#hours"><?php echo($lang("hours")); ?></a></li>
+                            <li><a href="/Information.php#location"><?php echo($lang("location")); ?></a></li>
+                            <li><a href="/Information.php#rules"><?php echo($lang("rules")); ?></a></li>
+                            <li><a href="/Information.php#registration"><?php echo($lang("registration")); ?></a></li>
+                            <li><a href="/Information.php#weapon"><?php echo($lang("weapon")); ?></a></li>
+                            <li><a href="/Information.php#autograph"><?php echo($lang("autograph")); ?></a></li>
                         </ul>
                     </li>
-                    <li><a href="/Admission.php"><span class="glyphicon glyphicon-bookmark"></span> Admission</a></li>
-                    <li><a href="/Guests.php"><span class="glyphicon glyphicon-star"></span> Guests</a></li>
-                    <li><a href="/Events.php"><span class="glyphicon glyphicon-bullhorn"></span> Events</a></li>
+                    <li><a href="/Admission.php"><span class="glyphicon glyphicon-bookmark"></span> <?php echo($lang("admission")); ?></a></li>
+                    <li><a href="/Guests.php"><span class="glyphicon glyphicon-star"></span> <?php echo($lang("guests")); ?></a></li>
+                    <li><a href="/Events.php"><span class="glyphicon glyphicon-bullhorn"></span> <?php echo($lang("events")); ?></a></li>
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="navButtons">
-                            <span class="glyphicon glyphicon-flag"></span> Exhibitions
+                            <span class="glyphicon glyphicon-flag"></span> <?php echo($lang("exhibitions")); ?>
                             <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="/Exhibitions.php#retro">Retro Section</a></li>
-                                <li><a href="/Exhibitions.php#arcade">Arcade Game</a></li>
-                                <li><a href="/Exhibitions.php#lan">LAN Party</a></li>
-                                <li><a href="/Exhibitions.php#aaa">AAA Studios</a></li>
-                                <li><a href="/Exhibitions.php#indie">Indie studios</a></li>
+                                <li><a href="/Exhibitions.php#retro"><?php echo($lang("retro")); ?></a></li>
+                                <li><a href="/Exhibitions.php#arcade"><?php echo($lang("arcade")); ?></a></li>
+                                <li><a href="/Exhibitions.php#lan"><?php echo($lang("lan")); ?></a></li>
+                                <li><a href="/Exhibitions.php#aaa"><?php echo($lang("aaa")); ?></a></li>
+                                <li><a href="/Exhibitions.php#indie"><?php echo($lang("indie")); ?></a></li>
                             </ul>
                         </li>
                     </ul>
@@ -66,7 +66,7 @@
                     <ul class="nav navbar-nav navbar-right">
                         <?php
                         if (!(func::checkLogin($con))) {
-                            echo "<li><a href='/Login_Register.php'><span class='glyphicon glyphicon-user'></span> Sign Up / Login</a></li>";
+                            echo "<li><a href='/Login_Register.php'><span class='glyphicon glyphicon-user'></span> " .$lang("signup_login") ."</a></li>";
                         } else {
                             echo "<li class=\"dropdown\">
                             <a class=\"dropdown - toggle\" data-toggle=\"dropdown\" href=\"#\" id=\"navButtons\">
@@ -79,7 +79,7 @@
                             </li >";
                         }
                         ?>
-                        <li><a style="margin-right: 30px" href="/Purchase/SetTickets.php">GET TICKET</a></li>
+                        <li><a style="margin-right: 30px" href="/Purchase/SetTickets.php"><?php echo($lang("get_ticket")); ?></a></li>
                     </ul>
                 </div>
             </div>
