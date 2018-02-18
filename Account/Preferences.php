cfecd7d3851be727891e6105d6e9eaa0c1bfb5d5
wfournier <wfournier2710@gmail.com>
2018-02-18 17:45:30 -0500

    preferences page translated

--- Account/Preferences.php
+++ Account/Preferences.php
@@ -27,28 +27,28 @@ if (!func::checkLogin($con)) {
             <div class="col-sm-3"></div>
             <div class="col-sm-6">
                 <div class="row">
-                    <p>This section is optional to fill out</p>
+                    <p><?php echo($lang("optional_msg")); ?></p>
                 </div>
                 <hr>
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="form">
                     <div class="row">
                         <div class="form-group col-xs-6">
-                            <label for="badgeName" class="<?php echo $badgeNameErr ? 'error' : '' ?>">Preferred Badge Name</label>
-                            <input type="text" class="form-control" id="badgeName" name="badgeName" value="<?php echo $badgeName ?>" maxlength="20" data-toggle="tooltip" title="Your badge name will appear on your ticket (No special characters allowed)">
+                            <label for="badgeName" class="<?php echo $badgeNameErr ? 'error' : '' ?>"><?php echo($lang("pref_badge_name")); ?></label>
+                            <input type="text" class="form-control" id="badgeName" name="badgeName" value="<?php echo $badgeName ?>" maxlength="20" data-toggle="tooltip" title="<?php echo($lang('tip_pref_badge_name')); ?>">
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-xs-6">
-                            <label for="emergContact" class="<?php echo $emergContactErr ? 'error' : '' ?>">Emergency Contact Phone #</label>
-                            <input type="tel" class="form-control" id="emergContact" name="emergContact" value="<?php echo $emergContact ?>" data-toggle="tooltip" title="Phone # to be called in case of emergency (i.e. 000-000-0000)">
+                            <label for="emergContact" class="<?php echo $emergContactErr ? 'error' : '' ?>"><?php echo($lang("emerg_contact_phone")); ?></label>
+                            <input type="tel" class="form-control" id="emergContact" name="emergContact" value="<?php echo $emergContact ?>" data-toggle="tooltip" title="<?php echo($lang('tip_emerg_contact_phone')); ?>">
                         </div>
                         <div class="form-group col-xs-6">
-                            <label for="emergContactName" class="<?php echo $emergContactNameErr ? 'error' : '' ?>">Emergency Contact Full Name</label>
-                            <input type="tel" class="form-control" id="emergContactName" name="emergContactName" value="<?php echo $emergContactName ?>" data-toggle="tooltip" title="Full name of the person to be called in case of emergency (This should be filled if an Emergency Contact Phone # is entered)">
+                            <label for="emergContactName" class="<?php echo $emergContactNameErr ? 'error' : '' ?>"><?php echo($lang("emerg_contact_name")); ?></label>
+                            <input type="tel" class="form-control" id="emergContactName" name="emergContactName" value="<?php echo $emergContactName ?>" data-toggle="tooltip" title="<?php echo($lang('tip_emerg_contact_name')); ?>">
                         </div>
                     </div>
                     <div class="row">
-                        <button type="submit" class="btn btn-success btn-lg">Save</button>
+                        <button type="submit" class="btn btn-success btn-lg"><?php echo($lang("save")); ?></button>
                     </div>
                     <div class="row" style="height: 80px; padding: 10px;">
                         <p><span class="success"><?php echo($prefOutput); ?></span></p>
