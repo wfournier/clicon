00a893944846ac3cb18053c9c3a7fa15740fdb37
SOMEONEPC\luxor <luxor37@gmail.com>
2018-02-18 21:21:12 -0500

    test5

--- Processes/Functions.php
+++ Processes/Functions.php
@@ -94,7 +94,7 @@ class func
     {
         include $_SERVER['DOCUMENT_ROOT'] . "/Classes/Transaction.php";
         $account_id = $_COOKIE['account_id'];
-        $query = "INSERT INTO transaction (TRANSACTION_ID, ACCOUNT_ID, PRICE_TOTAL, PURCHASE_DATE, ID_TOKEN) VALUES (null, '" . $account_id . "', " . $priceTotal . ", null, '" . $token . "');";
+        $query = "INSERT INTO transaction (TRANSACTION_ID, ACCOUNT_ID, PRICE_TOTAL, ID_TOKEN) VALUES (null, '" . $account_id . "', " . $priceTotal . ", '" . $token . "');";
         $results = self::getConnection()->query($query) or die ("HELP " . self::getConnection()->error);
     }
 
