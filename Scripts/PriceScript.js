e23a5f6edd4987b9ca93745c4e964bdc38d18a0a
SOMEONEPC\luxor <luxor37@gmail.com>
2018-02-21 23:44:19 -0500

    pricescript

--- Scripts/PriceScript.js
+++ Scripts/PriceScript.js
@@ -1,26 +1,73 @@
-var price = $('#price1').val();
+var price = parseInt($('#price1').text(), 10);
+if(isNaN(price)){
+    price = 0;
+}
 $(document).ready(function ()
 {
     $('#friday').click(function () {
         if ($(this).is(':checked')) {
-            price += 25;
-            $('#price1').val(price);
+            price = 25;
+            if ($('#saturday').is(':checked'))
+                price = 65;
+            if ($('#sunday').is(':checked'))
+                price = 60;
+            if ($('#sunday').is(':checked') && $('#saturday').is(':checked'))
+                price = 55;
+            $('#price1').text(price);
         }
         if (!$(this).is(':checked')) {
-            price -= 25;
-            $('#price1').val(price);
+            price = 0;
+            if ($('#saturday').is(':checked'))
+                price = 40;
+            if ($('#sunday').is(':checked'))
+                price = 35;
+            if ($('#sunday').is(':checked') && $('#saturday').is(':checked'))
+                price = 75;
+            $('#price1').text(price);
         }
     });
     $('#saturday').click(function () {
         if ($(this).is(':checked')) {
-            price += 40;
-            $('#price1').val(price);
+            price = 40;
+            if ($('#friday').is(':checked'))
+                price = 65;
+            if ($('#sunday').is(':checked'))
+                price = 75;
+            if ($('#sunday').is(':checked') && $('#friday').is(':checked'))
+                price = 55;
+            $('#price1').text(price);
+        }
+        if (!$(this).is(':checked')) {
+            price = 0;
+            if ($('#friday').is(':checked'))
+                price = 25;
+            if ($('#sunday').is(':checked'))
+                price = 35;
+            if ($('#sunday').is(':checked') && $('#friday').is(':checked'))
+                price = 60;
+            $('#price1').text(price);
         }
     });
     $('#sunday').click(function () {
         if ($(this).is(':checked')) {
-            price += 35;
-            $('#price1').val(price);
+            price = 35;
+            if ($('#saturday').is(':checked'))
+                price = 75;
+            if ($('#friday').is(':checked'))
+                price = 60;
+            if ($('#friday').is(':checked') && $('#saturday').is(':checked'))
+                price = 55;
+            $('#price1').text(price);
+        }
+        if (!$(this).is(':checked')) {
+            price = 0;
+            if ($('#saturday').is(':checked'))
+                price = 40;
+            if ($('#friday').is(':checked'))
+                price = 25;
+            if ($('#friday').is(':checked') && $('#saturday').is(':checked'))
+                price = 65;
+            $('#price1').text(price);
         }
     });
 });
